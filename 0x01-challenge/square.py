566ae56b188ea55c961aba9a3418c20d122f0d81
younesse el-mouden <dayouness187@gmail.com>
2023-10-15 22:10:06 +0000

    My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,7 +4,7 @@ square Modle
 """
 
 
-class square:
+class square():
     """
     square
     """
@@ -36,3 +36,4 @@ if __name__ == "__main__":
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
+
